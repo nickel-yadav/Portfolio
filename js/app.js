1967c970559eff962966053d2c8d5b2cbbe1d73a
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-23 17:10:15 +0530

    Portfolio page setup

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-// Event handler for preloader
+//============= Event handler for preloader =====================//
 const preloader = document.querySelector('.preloader');
 
 window.addEventListener('load', function() {
     preloader.classList.add('hide-preloader');
 });
 
-
+//============= Tab switching for portfolio ===================//
+const tab_buttons = document.querySelectorAll('tab');
 

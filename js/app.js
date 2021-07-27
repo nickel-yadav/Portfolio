be79cfe26a64f5e6d9acf16928c6c9a2ce17e437
Nikhil Yadav <nickelyadav57@gmail.com>
2021-07-27 20:33:30 +0530

    Added responsive sections and footer

--- js/app.js
+++ js/app.js
@@ -1,28 +1,9 @@
-//============= Event handler for preloader =====================//
-// const preloader = document.querySelector('.preloader');
-
-// window.addEventListener('load', function() {
-//     preloader.classList.add('hide-preloader');
-// });
-
-//============= Tab switching for portfolio ===================//
-const tab_buttons = document.querySelectorAll('tab');
-
 // ================= Responsive navbar =====================//
 $(document).ready(function() {
     $(".menu-icon").on("click", function() {
           $("nav ul").toggleClass("showing");
     });
+    $("nav ul li").on("click", function() {
+            $("nav ul").toggleClass("showing");
+    });
 });
-
-// Scrolling Effect
-
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
-    }
-
-    else {
-          $('nav').removeClass('black');
-    }
-})
